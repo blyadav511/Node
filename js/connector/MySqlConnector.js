d2c6f7ed7b985587a8fad6fc7fcaf6e005059aec
Babulal Yadav <blyadav511@gmail.com>
2016-12-26 00:20:35 +0530

    mysql connector

---
+++ js/connector/MySqlConnector.js
@@ -0,0 +1,44 @@
+"use strict";
+
+let mysql = require("mysql2");
+let _ reruire("underscore");
+
+class MySqlConnector {
+
+	static init(config){
+		this.pools  = {};
+		_.each(config, function(dbConfig, dbName) {
+			this.pools[dbName] = MySqlConnector.createPool(dbConfig);
+		}, this);
+	}
+
+	static getConnection(name, callback){
+		if(this.pools === null){
+			return callback(new Error("No such db in pool"));
+		}
+		return this.pools[name].getConnection(callback);
+	}
+
+	static createPool(connectionConfig){
+		return mysql.createPool(connectionConfig);
+	} 
+}
+
+if(MySqlConnector.pools == undefined){
+	let dbConfig = {
+		"test" : {
+			connectionLimit : process.env.TEST_POOL_SIZE | 20,
+			host : process.env.TEST_HOST | "babulal",
+			port : process.env.TEST_PORT | 3600,
+			user : process,env.TEST_USER | "babulal",
+			password : process.env.TEST_PASS | null,
+			database : process.env.TEST_DATABASE | "test"
+		}
+	};
+	MySqlConnector.init(dbConfig);
+	_.each(MySqlConnector.pools, function(pool, name){
+		pool.on("enqueue", function(){
+			console.log(name + " Queuing for connection on pool");
+		});
+	});
+}
